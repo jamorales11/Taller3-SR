79a48689bed011b492bc3357f28c7747a0a8cba4
Julio Morales <ja.morales11@uniandes.edu.co>
2022-06-06 21:12:37 -0500

    Debug envio imagen x7

--- frontTaller/src/app/lista-recomendaciones/lista-recomendaciones.component.ts
+++ frontTaller/src/app/lista-recomendaciones/lista-recomendaciones.component.ts
@@ -14,15 +14,35 @@ export class ListaRecomendacionesComponent implements OnInit {
 
   usersImportantes : any[] = [];
 
+  imageToShow: any;
+  isImageLoading: any;
+
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+        this.imageToShow = reader.result;
+    }, false);
+
+    if (image) {
+        reader.readAsDataURL(image);
+    }
+  }
+
 
 
   constructor(private usuarioService: UsuarioService) { 
     this.usuarioService.get_recomendaciones_by_id(usuarioService.idLogged).subscribe((data:any)=>{
       console.log(data);
+      this.isImageLoading = true;
       this.usuarioService.get_grafo().subscribe((dataImg:any)=>{
         console.log(dataImg);
+        this.createImageFromBlob(dataImg);
+        this.isImageLoading = false;
 
-      })
+      }, error =>{
+        this.isImageLoading = false;
+        console.log(error);
+      });
       // console.log(JSON.parse(data["recommendaciones"]));
       // console.log(JSON.parse(data["usuarios"]));
       // console.log(data["features"]);
